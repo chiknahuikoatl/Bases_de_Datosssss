f241f8b26aba20024d148f249ad993500774e287
chiknahuikoatl <jalrod@ciencias.unam.mx>
2019-11-15 18:38:27 -0600

    Consultas f y j.

--- Tarea_6/Tarea06/Consultas/consultas.sql
+++ Tarea_6/Tarea06/Consultas/consultas.sql
@@ -7,31 +7,32 @@ SELECT nombre, ciudad
 -- f. Obtener una lista de cada compañía y el salario promedio que paga. La
 --  información se debe mostrar por compañía, año, y género.
 
-
--- procedure
--- sadasduñisdcn
-
 SELECT empresa, CASE genero
                     WHEN genero = 'M' THEN 'Masc',
                     ELSE 'Fem'
                   END AS Género,
-
+        año
     FROM (Empleado e JOIN Trabajar t ON t.CURP = e.CURP) JOIN
             Empresa em ON t.RFC = em.RFC
-    GROUP BY em.razonSocial, AVG(t.salarioQuincenal),
+    GROUP BY em.razonSocial, AVG(t.salarioQuincenal), YEAR(t.fechaIngreso);
 
-
-SELECT empresa,
-    FROM (Empleado e JOIN Trabajar t ON t.CURP = e.CURP) JOIN
-            Empresa em ON t.RFC = em.RFC
-    GROUP BY em.razonSocial, AVG(t.salarioQuincenal),
 
 -- j. Encontrar la cantidad de empleados en cada compañía, por año, trimestre y
 --  género.
-SELECT compañia, año, trimestre, genero, COUNT(CURP)
+SELECT compañia, DATEPART(y, t.fechaIngreso) año,
+        DATEPART(qq, t.fechaIngreso) trimestre, genero, COUNT(CURP)
     FROM (Empleado e JOIN Trabajar t ON t.CURP = e.CURP) JOIN
             Empresa em ON t.RFC = em.RFC
-    GROUP BY em.razonSocial, e.genero
+    GROUP BY em.razonSocial, DATEPART(y, t.fechaIngreso),
+            DATEPART(qq, t.fechaIngreso), e.genero;
+
+--n. Información de los proyectos en los que colaboran los empleados que son
+--  directores.
+
+SELECT
+    FROM (Dirigir d INNER JOIN Colaborar c ON d.CURP = c.CURP) INNER JOIN
+            Proyecto p ON p.numProyecto = c.numProyecto
+    WHERE
 
 
---nrv
+--rv
